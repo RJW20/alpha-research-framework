772605827389b220deae4d54d5f533fdd49d1735
RJW20 <robertjwatts84@outlook.com>
2026-02-19 15:38:41 +0000

    Adjust evaluate function to only evaluate against horizons at frequency equal to the size of the horizon

--- alpha_research_framework/evaluate.py
+++ alpha_research_framework/evaluate.py
@@ -3,6 +3,7 @@ import pandas as pd
 from alpha_research_framework.alphas import Alpha
 from alpha_research_framework.spearman_rank import spearman_rank
 from alpha_research_framework.universe import Universe
+from alpha_research_framework.window import Window
 
 
 def evaluate(universe: Universe, alphas: list[Alpha]) -> pd.DataFrame:
@@ -12,8 +13,9 @@ def evaluate(universe: Universe, alphas: list[Alpha]) -> pd.DataFrame:
 
     Returns a pd.DataFrame indexed by date, with one column per (alpha, horizon)
     pair, containing the cross-sectional correlation between alpha scores at
-    time t and forward returns at time t + horizon
-    Missing values may appear where:
+    time t and forward returns between times t + 1 and t + horizon.
+    Missing values will appear where:
+    - frequency does not align with horizon
     - alpha signals cannot be computed (start of sample)
     - forward returns cannot be computed (end of sample)
     - the cross-section is too small after filtering
@@ -25,12 +27,21 @@ def evaluate(universe: Universe, alphas: list[Alpha]) -> pd.DataFrame:
 
     features = set().union(*[alpha.required_features for alpha in alphas])
     universe.build_features(features)
-    for date in universe.dates:
+    horizons = {h for a in alphas for h in a.HORIZONS}
+    for t, date in enumerate(universe.dates):
+
+        valid_horizons = {w for w in Window if not t % w.value} & horizons
+        if not valid_horizons:
+            continue
+
         x = universe.cross_section(date)
         fut_ret = universe.future_returns(date)
         for alpha in alphas:
+            horizons_to_evaluate = valid_horizons & alpha.HORIZONS
+            if not horizons_to_evaluate:
+                continue
             signal = alpha.compute(x)
-            for horizon in alpha.HORIZONS:
+            for horizon in horizons_to_evaluate:
                 ic_df.loc[date, (alpha.NAME, horizon)] = spearman_rank(
                     signal, fut_ret[horizon]
                 )
