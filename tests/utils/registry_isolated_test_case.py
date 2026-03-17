06a3563a4d4a31239d4ea3b39e9a4a38343a8afd
RJW20 <robertjwatts84@outlook.com>
2026-03-17 17:10:06 +0000

    Update RegistryIsolatedTestCase to use Registrable instead of Operator

--- tests/utils/registry_isolated_test_case.py
+++ tests/utils/registry_isolated_test_case.py
@@ -2,18 +2,18 @@ import unittest
 from typing import ClassVar
 
 from alpha_research_framework.class_var_validator import ClassVarValidator
-from alpha_research_framework.operator import Operator
+from alpha_research_framework.registrable import Registrable
 
 
 class RegistryIsolatedTestCase(unittest.TestCase, ClassVarValidator):
     """
-    Class for ensuring a `registry_root=True` operator's `__registry__`
+    Class for ensuring a `registry_root=True` registrable's `__registry__`
     class variable is not permanently mutated during tests.
 
     All subclasses defined in one test scope must still have unique names.
     """
 
-    REGISTRY_OWNER: ClassVar[type[Operator]]
+    REGISTRY_OWNER: ClassVar[type[Registrable]]
 
     def __init_subclass__(cls) -> None:
         cls.assert_class_var(name="REGISTRY_OWNER", type=type)
