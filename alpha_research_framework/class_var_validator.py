3ec33cf35853c639ced22aca382b2af106d00ab1
RJW20 <robertjwatts84@outlook.com>
2026-03-18 14:48:39 +0000

    Add subtype assertions to ClassVarValidator

--- alpha_research_framework/class_var_validator.py
+++ alpha_research_framework/class_var_validator.py
@@ -11,11 +11,12 @@ class ClassVarValidator:
     def assert_class_var(
         cls,
         name: str,
+        *,
         type: type,
         bad_values: set[Any] | None = None,
     ) -> None:
         """
-        Assert that a class variable exists, has the right type, and is not in
+        Assert that a class variable exists, is of type `type` and is not in
         `bad_values`.
         """
 
@@ -23,41 +24,85 @@ class ClassVarValidator:
             raise AttributeError(
                 f"{cls.__name__} must define class variable '{name}'"
             )
-        value = getattr(cls, name)
-        if not isinstance(value, type):
+        class_var = getattr(cls, name)
+        if not isinstance(class_var, type):
             raise TypeError(
                 f"{cls.__name__}.{name} must be of type {type.__name__}, got "
-                f"{value.__class__.__name__}"
+                f"{class_var.__class__.__name__}"
             )
-        if bad_values and value in bad_values:
+        if bad_values and class_var in bad_values:
             raise ValueError(
                 f"{cls.__name__}.{name} cannot be one of {bad_values}"
+            )
+        
+    @classmethod
+    def assert_class_var_subtype(cls, name: str, *, base_type: type) -> None:
+        """
+        Assert that a class variable exists, is of type `type` and is a subclass
+        of `base_type`.
+        """
+
+        cls.assert_class_var(name, type=type)
+
+        class_var = getattr(cls, name)
+        if not issubclass(class_var, base_type):
+            raise TypeError(
+                f"{cls.__name__}.{name} must be a subclass of type "
+                f"{base_type.__name__}"
             )
 
     @classmethod
     def assert_class_var_container(
         cls,
         name: str,
+        *,
         container_type: type,
         element_type: type,
         bad_values: set[Any] | None = None,
     ) -> None:
         """
-        Assert that a class variable exists, is a container of the right type,
-        contains only elements of the correct type, and contains no element in
-        `bad_values`.
+        Assert that a class variable exists, is of type `container_type`,
+        contains only `element_type`, and contains no element in `bad_values`.
         """
 
-        cls.assert_class_var(name, container_type)
+        cls.assert_class_var(name, type=container_type)
 
-        value = getattr(cls, name)
-        for i, element in enumerate(value):
+        class_var = getattr(cls, name)
+        for element in class_var:
             if not isinstance(element, element_type):
                 raise TypeError(
-                    f"{cls.__name__}.{name}[{i}] must be "
+                    f"{cls.__name__}.{name} must exclusively contain type "
                     f"{element_type.__name__}, got {type(element).__name__}"
                 )
             if bad_values and element in bad_values:
                 raise ValueError(
-                    f"{cls.__name__}.{name}[{i}] cannot be one of {bad_values}"
+                    f"{cls.__name__}.{name} cannot contain one of {bad_values}"
+                )
+
+    @classmethod
+    def assert_class_var_container_of_subtype(
+        cls,
+        name: str,
+        *,
+        container_type: type,
+        element_base_type: type,
+    ) -> None:
+        """
+        Assert that a class variable exists, is of type `container_type` and
+        contains elements of type `type` which are all subclasses of
+        `element_base_type`.
+        """
+
+        cls.assert_class_var_container(
+            name,
+            container_type=container_type,
+            element_type=type,
+        )
+
+        class_var = getattr(cls, name)
+        for element in class_var:
+            if not issubclass(element, element_base_type):
+                raise TypeError(
+                    f"{cls.__name__}.{name} must exclusively contain "
+                    f"subclasses of type {element_base_type.__name__}"
                 )
--- tests/test_class_var_validator.py
+++ tests/test_class_var_validator.py
@@ -9,7 +9,7 @@ class TestAssertClassVar(unittest.TestCase):
     class NeedsClassVarValidated(ClassVarValidator):
         attr: ClassVar[str]
         def __init_subclass__(cls) -> None:
-            cls.assert_class_var(name="attr", type=str, bad_values={""})
+            cls.assert_class_var("attr", type=str, bad_values={""})
 
     def test_definition(self) -> None:
         """Verify definition of class var enforced."""
@@ -39,6 +39,40 @@ class TestAssertClassVar(unittest.TestCase):
                 attr = ""
 
 
+class TestAssertClassVarSubType(unittest.TestCase):
+
+    class NeedsClassVarValidated(ClassVarValidator):
+        attr: ClassVar[type[str]]
+        def __init_subclass__(cls) -> None:
+            cls.assert_class_var_subtype("attr", base_type=str)
+
+    def test_definition(self) -> None:
+        """Verify definition of class var enforced."""
+
+        with self.assertRaises(AttributeError):
+            class NeedsClassVarValidatedSubClass(
+                TestAssertClassVar.NeedsClassVarValidated
+            ):
+                pass
+
+    def test_type(self) -> None:
+        """Verify class var is a type enforced."""
+
+        class NeedsClassVarValidatedSubClass(
+                TestAssertClassVar.NeedsClassVarValidated
+            ):
+                attr = "a"
+
+    def test_base_type(self) -> None:
+        """Verify base type of class var enforced."""
+
+        with self.assertRaises(TypeError):
+            class NeedsClassVarValidatedSubClass(
+                TestAssertClassVar.NeedsClassVarValidated
+            ):
+                attr = int
+
+
 class TestAssertClassVarContainer(unittest.TestCase):
 
     class NeedsClassVarContainerValidated(ClassVarValidator):
@@ -56,16 +90,16 @@ class TestAssertClassVarContainer(unittest.TestCase):
 
         with self.assertRaises(AttributeError):
             class NeedsClassVarValidatedSubClass(
-                TestAssertClassVar.NeedsClassVarValidated
+                TestAssertClassVarContainer.NeedsClassVarContainerValidated
             ):
                 pass
 
     def test_container_type(self) -> None:
         """Verify type of container class var enforced."""
 
-        with self.assertRaises(AttributeError):
+        with self.assertRaises(TypeError):
             class NeedsClassVarValidatedSubClass(
-                TestAssertClassVar.NeedsClassVarValidated
+                TestAssertClassVarContainer.NeedsClassVarContainerValidated
             ):
                 attrs = ["attr1", "attr2"]
 
@@ -86,6 +120,58 @@ class TestAssertClassVarContainer(unittest.TestCase):
                 TestAssertClassVarContainer.NeedsClassVarContainerValidated
             ):
                 attrs = {""}
+
+
+class TestAssertClassVarContainerOfSubType(unittest.TestCase):
+
+    class NeedsClassVarContainerValidated(ClassVarValidator):
+        attrs: ClassVar[set[type[str]]]
+        def __init_subclass__(cls) -> None:
+            cls.assert_class_var_container_of_subtype(
+                name="attrs",
+                container_type=set,
+                element_base_type=str,
+            )
+
+    def test_definition(self) -> None:
+        """Verify definition of container class var enforced."""
+
+        with self.assertRaises(AttributeError):
+            class NeedsClassVarValidatedSubClass(
+                TestAssertClassVarContainerOfSubType
+                .NeedsClassVarContainerValidated
+            ):
+                pass
+
+    def test_container_type(self) -> None:
+        """Verify type of container class var enforced."""
+
+        with self.assertRaises(TypeError):
+            class NeedsClassVarValidatedSubClass(
+                TestAssertClassVarContainerOfSubType
+                .NeedsClassVarContainerValidated
+            ):
+                attrs = [str, str]
+
+    def test_element_type(self) -> None:
+        """Verify container class var elements are types enforced."""
+
+        with self.assertRaises(TypeError):
+            class NeedsClassVarContainerValidatedSubClass(
+                TestAssertClassVarContainerOfSubType
+                .NeedsClassVarContainerValidated
+            ):
+                attrs = {"a"}
+
+    def test_element_base_type(self) -> None:
+        """Verify base type of container class var elements enforced."""
+
+        with self.assertRaises(TypeError):
+            class NeedsClassVarContainerValidatedSubClass(
+                TestAssertClassVarContainerOfSubType
+                .NeedsClassVarContainerValidated
+            ):
+                attrs = {int}
 
 
 if __name__ == "__main__":
