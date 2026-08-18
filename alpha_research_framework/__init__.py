6f4914161d593e6b36a02000f535c7a8d5b5657a
RJW20 <robertjwatts84@outlook.com>
2026-08-18 11:40:11 +0100

    Add __all__ to top __init__.py

--- alpha_research_framework/__init__.py
+++ alpha_research_framework/__init__.py
@@ -3,3 +3,11 @@ from .download import download
 from .evaluate import evaluate
 from .universe import Universe, build_universe_for
 from .window import Window
+
+__all__ = [
+    "alphas", "signals", "series", "metrics",
+    "download",
+    "evaluate",
+    "Universe", "build_universe_for",
+    "Window"
+]
